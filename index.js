5282fedb83a33b04ab3ab901e5b4a8db63d177a8
Jesper Hermansen <jesper@viggo.dk>
2020-06-12 13:57:25 +0200

    Rewrote fork from gulp-css-buster

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 'use strict';
 
 // Dependencies
-var crypto = require('crypto')
-var fs = require('fs');
-var gutil = require('gulp-util');
-var through = require('through2');
+const fs = require('fs');
+const gutil = require('gulp-util');
+const through = require('through2');
 
 // Consts
-var PLUGIN_NAME = 'gulp-css-buster';
+var PLUGIN_NAME = 'gulp-css-url-bust';
 
 /**
  * Add md5 hash for cache baster to local URL in css.
@@ -45,19 +44,25 @@ module.exports = function(options) {
  * @param  {Function} cb         function (err, file)
  */
 function transform(file, assetsPath, cb) {
-    var regex = new RegExp("url[(]['\"]?([^'\")]*)['\"]?[)]", "g");
+    const regex = /url\((['\"]?)(.*?)\1\)/g;
 
     var contents = file.contents.toString('utf8');
 
     asyncReplace(contents, regex, function(match, callback) {
-        var file = (match[1].charAt(0) == '/') ? match[1] : '/' + match[1];
+        if (match[2].match(/^(?:https?:\/\/|data:)/)) {
+            return callback(match[0]);
+        }
+        var file = (match[2].charAt(0) == '/') ? match[2] : '/' + match[2];
+
+        file = file.split(/[#\?]/)[0];
 
         md5File(assetsPath + file, function(err, hash) {
             // if md5 fails (ex: file not found), don't replace the url
             if (err) {
+                console.log(`Missing url in less: ${match[2]}`);
                 return callback(match[0]);
             }
-            callback("url('" + makeUrlWithHash(match[1], hash) + "')");
+            callback(`url(${match[1]}${makeUrlWithHash(match[2], hash)}${match[1]})`);
         });
     }, function(err, newContent) {
         file.contents = new Buffer(newContent);
@@ -102,27 +107,20 @@ function asyncReplace(string, regex, replacer, done) {
  * @return {string}
  */
 function makeUrlWithHash(url, hash) {
-    var filename = url;
-    var extension = '';
-
-    var slashPos = url.lastIndexOf('/');
-    if (slashPos == -1) {
-        var dotPos = url.lastIndexOf('.');
-        if (dotPos != -1) {
-            filename = url.substr(0, dotPos);
-            extension = url.substr(dotPos);
+    let poundSign = url.split('#');
+    url = poundSign[0];
+    if (url.indexOf('.') != -1) {
+        if (url.indexOf('?') != -1) {
+            url += '&';
+        } else {
+            url += '?';
         }
-    } else {
-        var path = url.substr(0, slashPos);
-        var file = url.substr(slashPos);
-        var dotPos = file.lastIndexOf('.');
-        if (dotPos != -1) {
-            filename = path + file.substr(0, dotPos);
-            extension = file.substr(dotPos);
+        url += 'mtime=' + hash;
+        if (poundSign[1]) {
+            url += '#' + poundSign[1];
         }
     }
-
-    return filename + '-' + hash + extension;
+    return url;
 }
 
 /**
@@ -131,18 +129,17 @@ function makeUrlWithHash(url, hash) {
  * @param  {Function} cb       function(err, hash)
  */
 function md5File(filename, cb) {
-    var output = crypto.createHash('md5')
-    var input = fs.createReadStream(filename)
+    fs.stat(filename, function(err, stat) {
+        if (err) {
+            cb(err);
+        } else {
+            cb(null, dateformat(stat.mtime));
+        }
+    });
+}
 
-    input.on('error', function(err) {
-        cb(err)
-    })
-
-    output.once('readable', function() {
-        cb(null, output.read().toString('hex'))
-    })
-
-    input.pipe(output)
+function dateformat(date) {
+    return date.getTime().toString(36);
 }
 
 /**
